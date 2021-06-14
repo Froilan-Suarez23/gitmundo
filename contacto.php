74c841485ad4b3d13cf2ff4c6d368298cf516a67
Frojez <froilan.surozz.6a97@gmail.com>
2021-06-14 16:19:58 -0500

    primer screenshot del proyecto GitMundo "Frojez"

---
+++ contacto.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GitMundo Contacto</title>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+<br>
+<div class="container">
+    	<div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card justify-content-center">
+                <div class="card-header">
+                	Froilán de Jesús Suárez Rodríguez
+                </div>
+                	
+                <div class="card-body  justify-content-center">
+                    <div class="container " >
+					  <h2>Redes Sociales</h2>
+					  <div class="" style="width: 100%">
+					  	<img class="card-img-top" src="imag/froilan.jpg" alt="Card image" style="width:40%">
+					  </div>
+
+
+
+					  <p>
+					  	Facebook: Froilan Suarez
+					  	<a href="http://www.facebook.com/froilandejesus.suarezrodriguez.9" target="_blank"><img src="imag/facebook.png"></a>
+					  </p>
+					  <p>
+					  	YOUTUBE: Frojez SuRoZ
+
+					  	<a href="https://www.youtube.com/channel/UCbfW57RWW5XD8tyG6XvuvMA" target="_blank"><img src="imag/youtube.png"></a>
+
+					  </p>
+
+					  <p>
+					  	INSTAGRAM: suarez_froilan
+
+					  <a href="https://www.instagram.com/suarez_froilan/"><img src="imag/instagram.png"></a>
+					  </p>
+
+					  <P>
+					  WHATSAPP: 9612784837
+					  <a href=""><img src="imag/whatsapp.png"></a>
+					  </P>
+
+					  <p>
+					  	CORREO: froilan.surozz.6a97@gmail.com
+					  	<a href=""><img src="imag/gmail.png"></a>
+					  </p>
+					  
+					  
+
+					  
+					<a href="index.php" class="btn btn-primary">Regresar</a>
+					</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GitMundo</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<br>
+
+	<div class="container">
+    	<div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"></div>
+
+                <div class="card-body">
+                    
+                </div>
+
+                <div class="card-body">
+                    <div class="container">
+         
+                      <br>
+                      <!-- Nav pills -->
+                      <ul class="nav nav-pills" role="tablist">
+                        <li class="nav-item">
+                          <a class="nav-link active" data-toggle="pill" href="#home">INDICE</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link"  href="contacto.php">CONTACTO</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" data-toggle="pill" href="#menu2">INFORMACIÓN</a>
+                        </li>
+                      </ul>
+
+                      <!-- Tab panes -->
+                      <div class="tab-content">
+                        <div id="home" class="container tab-pane active"><br>
+                          <h3>INDICE</h3>
+                          <p class="justify-content-center">En la materia de web integral se realiza una aplicación de votos en línea para los alumnos de la universidad, 
+en dicha aplicación el alumno con su matrícula podrá ejercer su voto en algunos aspectos propios de la 
+universidad, tales como el proceso de elección de estudiantes (Consejo estudiantil, representantes de 
+carreras, etc.), proyectos universitarios de su división o por consecuente de la universidad, la elección de 
+grupos de trabajos y/o profesores del plantel. Buscando que los alumnos interactúan con la tecnología y 
+agilizar estos procesos dentro de nuestra unidad académica  
+<br>
+<br>
+En la materia de dispositivos inteligentes, se buscará desarrollar aplicaciones con dispositivos wearables, 
+en esta unidad se comenzó a trabajar con un Smart watch, debido a que la materia es relativamente nueva 
+en nuestra unidad académica, aun no se tiene claro el proyecto final, pero el objetivo es que los alumnos 
+puedan desarrollar aplicaciones para cualquier dispositivo wearable  </p>
+                        </div>
+                        
+                        <div id="menu2" class="container tab-pane fade"><br>
+                          <h3>INFORMACIÓN</h3>
+                          <p>UNIVERSIDAD TECNOLÓGICA DE LA SELVA.
+                          	<BR>
+                          	INGENIERÍA EN DESARROLLO Y GESTIÓN DE SOFTWARE
+                          	<BR>
+                          	DESARROLLO WEB INTEGRAL
+                          	<BR>
+                       
+                          	
+                          	DOCENTE
+                          	<BR>
+                          	HECTOR HUGO HERRERA ESCOBAR
+
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
