ee4f8392aa1445cd61570ce6cfd3085275a8d8f6
091810686 Froilan <sofimosol@gmail.com>
2021-06-20 23:41:51 -0500

    SNAPSTHOT 002 del Proyecto, solo mdi

--- index.php
+++ index.php
@@ -18,10 +18,10 @@
     	<div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"></div>
+                <div class="card-header"><h1>Hola TICsitos</h1></div>
 
                 <div class="card-body">
-                    
+                   
                 </div>
 
                 <div class="card-body">
